61e35f3c17505341d68217d869ed4efe5ed1a16c
Fred Sladkey <fsladkey@gmail.com>
2017-03-12 17:49:43 -0400

    Fix typo in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import rootReducer from './reducers/root_reducer';
+import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
-if (node.process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   middlewares.push(createLogger())
 }
 
