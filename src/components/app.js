ad4a88fe2e0cfee2ceca8814bac4566baa1a140a
Fred Sladkey <fsladkey@gmail.com>
2017-03-12 17:48:37 -0400

    Add shared styles and flesh out main app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import SearchOptions from './search_options';
+import JobList from './job_list';
+import TableHeaders from './table_headers';
+import URLPreview from './url_preview';
+import Logo from './logo';
 import styles from './styles/shared.scss';
-
 
 export default function App(props) {
   return (
     <main>
-      <h1>Hello</h1>
+      <Logo />
+      <h1>Search Jobs</h1>
+      <URLPreview />
+      <section className="search-options">
+        <SearchOptions />
+      </section>
+      <TableHeaders />
+      <JobList />
     </main>
-  );
+  )
 }
